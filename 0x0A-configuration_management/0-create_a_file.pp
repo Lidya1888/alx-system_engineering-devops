a2a519582b77a0b859e3fbb988e7551cce0b33ea
Lidya188 <lidyagenene@gmail.com>
2022-09-16 15:35:30 -0400

    all

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
-#creates a file in the tmp dir of the node
+# Creates a file in /tmp
 
-file { 'holberton':
-  path    => '/tmp/holberton',
+file { 'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+  path    => '/tmp/school',
 }
